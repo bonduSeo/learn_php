3b2fe4b0aac8b5e66c9baccb759c0243ae758802
bondu_green <bondu.seo@gmail.com>
2022-04-07 13:21:30 +0900

    0407 lunch

--- ch04/08.for_mission9_another.php
+++ ch04/08.for_mission9_another.php
@@ -1,8 +1,11 @@
 <?php
+
+// 승재씨코드
     $star = rand(3,20);
   
     print  $star."<br>";
 
+    $sum = ""; //php에선 안해도되지만 요거 해주는 습관을 들입시다
     for ($z=1;$z <= $star; $z++)
     {
         $sum = $sum."@";
---
+++ ch04/09.for_mission10.php
@@ -0,0 +1,42 @@
+<?php
+    $val = rand(3,10);
+    $num = 1;
+    echo $val;
+    echo "<table>";
+    
+    for($i=1;$i<=$val;$i++)
+    {
+        echo "<tr>";
+            for($j=1;$j<=$val;$j++)
+            {
+                echo "<td>";
+             //   echo "<div id=nm>".$j+$val*($i-1) ."</div>";
+                echo "<div id=nm>".$num++."</div>";
+              
+                echo "</td>";
+            }
+
+
+        echo "</tr>";
+
+
+    }
+    echo "</table>";
+?>
+
+<style>
+    /* * {margin:0; padding:0;} */
+    table {
+        border-collapse : collapse;
+    }
+    table, tr, td {
+        border : 1px solid black;
+    }
+    td {
+        height: 40px;
+        width: 40px;
+        text-align:center;
+ 
+        }
+   
+</style>
