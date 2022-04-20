06517f9652de3a946dbc4da8758e5971cb0c8f0a
bondu_green <bondu.seo@gmail.com>
2022-04-20 17:32:22 +0900

    hig

--- practice/img_board/upload.php
+++ practice/img_board/upload.php
@@ -1,63 +1,19 @@
 <?php
+    session_start();
    include_once('db_connect.php');
 
   
    $title = $_POST["title"];
-   $account = $_POST["account"];
-
-    $target_dir = "../../uploads/";
-    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-    // Check if image file is a actual image or fake image
-    if(isset($_POST["submit"])) {
-        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-        if($check !== false) {
-            echo "File is an image - " . $check["mime"] . ".";
-            $uploadOk = 1;
-        } else {
-            echo "File is not an image.";
-            $uploadOk = 0;
-        }
-    }
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        // echo "Sorry, file already exists.";
-        while(file_exists($target_file)) {
-            $target_file = $target_file."1";
-        }
-     
-        // $uploadOk = 0;
-    }
-    // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 5000000) {
-        echo "Sorry, your file is too large.";
-        $uploadOk = 0;
-    }
-    // Allow certain file formats
-    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-    && $imageFileType != "gif" ) {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-        $uploadOk = 0;
-    }
-    // Check if $uploadOk is set to 0 by an error
-    if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
-        die();
-    // if everything is ok, try to upload file
+    if(isset($_SESSION['log_status'])) {
+        $account = $_SESSION['log_id'];
     } else {
-        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-            // echo "<p>The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.</p>";
-            // echo "<br><img src=../../uploads/".basename( $_FILES["fileToUpload"]["name"]). ">";
-            // echo "<br><button type='button' onclick='history.back()'>돌아가기</button>";
-        } else {
-            echo "<p>Sorry, there was an error uploading your file.</p>";
-            echo "<br><button type='button' onclick='history.back()'>돌아가기</button>";
-            die();
-        }
+        header('Location:list.php');
     }
-
-    $img_url = "../../uploads/".basename( $_FILES['fileToUpload']['name']);
+    $fileToUpload = $_FILES["fileToUpload"];
+    // print_r($fileToUpload);
+    // die();
+    $img_url = upload_file($fileToUpload);
+    //
     $sql = 
    "
        INSERT INTO gallery3
